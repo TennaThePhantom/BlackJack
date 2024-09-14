517f68af9beda99d0409a70999d98635de52e784
Tennessee <tennatenna99@outlook.com>
2024-09-14 17:31:57 -0400

    started working on start page

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 // main javascript code the home page
 import * as credits from './credits.js';
 import * as settings from './settings.js'
-const start = ""; // get id from start
+const start = document.getElementById("start"); // get id from start
+const moneyHeader = document.createElement("h1")
+const moneyButton1 = document.createElement("button");
+const moneyButton2 = document.createElement("button");
+const moneyButton3 = document.createElement("button");
+const customMoney = document.createElement("button");
+
+
+
